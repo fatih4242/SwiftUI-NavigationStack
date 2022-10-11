d380a29aa2770b0db926f2ecdddefa63fdb4d83d
fatih <fatih16@live.nl>
2022-10-11 18:48:40 +0300

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Fatih Toker on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
